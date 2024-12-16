5edd23b072d9fcc572de595914e27705a8231080
SOS-Team <sosproject50@gmail.com>
2024-12-16 22:49:58 +0200

    Create installdb.sh

---
+++ installdb.sh
@@ -0,0 +1,7 @@
+apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc |   gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg    --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+apt-get update && apt-get install -y mongodb-org
+mkdir -p  /data/db
+chmod -R 777 /data/db
+mongod --fork --logpath /var/log/mongod.log --bind_ip 0.0.0.0
