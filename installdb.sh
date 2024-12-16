bee83ac253171f3592fa5e23b5088aae6bf47298
SOS-Team <sosproject50@gmail.com>
2024-12-16 23:08:17 +0200

    Update installdb.sh

--- installdb.sh
+++ installdb.sh
@@ -1,3 +1,9 @@
+# Define variables
+MONGO_HOST="localhost" # Change to your MongoDB host if not localhost
+MONGO_PORT="27017"     # Change to your MongoDB port if not default
+USERNAME="scuser"      # Replace with your desired username
+PASSWORD="pass"        # Replace with your desired password
+
 apt-get install gnupg curl
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc |   gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg    --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-8.0.list
@@ -5,3 +11,19 @@ apt-get update && apt-get install -y mongodb-org
 mkdir -p  /data/db
 chmod -R 777 /data/db
 mongod --fork --logpath /var/log/mongod.log --bind_ip 0.0.0.0
+#!/bin/bash
+
+
+# Execute the MongoDB command via mongosh
+mongosh --host $MONGO_HOST --port $MONGO_PORT <<EOF
+use admin;
+db.createUser({
+  user: "$USERNAME",
+  pwd: "$PASSWORD",
+  roles: [
+    { role: "root", db: "admin" }
+  ]
+});
+EOF
+
+echo "User '$USERNAME' created successfully with root role."
