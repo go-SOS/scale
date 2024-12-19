6c7d5778424a55f2085e9440a5e955e4c3f4225b
Mohamed Nasr <110223394+0xBugatti@users.noreply.github.com>
2024-12-19 20:37:41 +0200

    Create footer.jsx

---
+++ src/components/home/footer.jsx
@@ -0,0 +1,12 @@
+// src/components/home/Footer.jsx
+
+const footer = () => {
+    return (
+        <footer style={{ textAlign: 'center', padding: '1rem', background: '#f1f1f1' }}>
+            <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default footer;
+                          
