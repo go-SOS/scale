d2a3277c1b59aba2739f5dcf0b33a6bc29a9e8ee
Mohamed Nasr <110223394+0xBugatti@users.noreply.github.com>
2024-12-19 22:41:14 +0200

    Update installdb.sh

--- installdb.sh
+++ installdb.sh
@@ -1,8 +1,3 @@
-# Define variables
-MONGO_HOST="127.0.0.1" # Change to your MongoDB host if not localhost
-MONGO_PORT="27017"     # Change to your MongoDB port if not default
-USERNAME="scuser"      # Replace with your desired username
-PASSWORD="pass"        # Replace with your desired password
 
 apt-get install gnupg curl
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc |   gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg    --dearmor
@@ -10,27 +5,4 @@ echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gp
 apt-get update && apt-get install -y mongodb-org
 mkdir -p  /data/db
 chmod -R 777 /data/db
-mongod --fork --logpath /var/log/mongod.log --bind_ip 0.0.0.0
-#!/bin/bash
 
-
-# Execute the MongoDB command via mongosh
-mongosh --host $MONGO_HOST --port $MONGO_PORT <<EOF
-use scaledb;
-db.createUser({
-  user: "$USERNAME",
-  pwd: "$PASSWORD",
-
-
-  
-  roles: [
-
-    { role: "dbAdmin", db: "scaledb" },
-    { role: "userAdmin", db: "scaledb" },
-    { role: "readWrite", db: "scaledb" },
-    { role: "root", db: "admin" }
-  ]
-});
-EOF
-
-echo "User '$USERNAME' created successfully with root role."
