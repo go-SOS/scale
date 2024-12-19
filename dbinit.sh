4e2d90e64d3984c3693ee2523ff318af7546d61c
Mohamed Nasr <110223394+0xBugatti@users.noreply.github.com>
2024-12-19 22:34:02 +0200

    Update dbinit.sh

--- dbinit.sh
+++ dbinit.sh
@@ -3,7 +3,7 @@ MONGO_HOST="127.0.0.1" # Change to your MongoDB host if not localhost
 MONGO_PORT="27017"     # Change to your MongoDB port if not default
 USERNAME="scuser"      # Replace with your desired username
 PASSWORD="pass"        # Replace with your desired password
-
+mongod --fork --logpath /var/log/mongod.log --bind_ip 0.0.0.0
 # Wait for MongoDB to start
 until mongosh --host localhost --port 27017 --eval "print('waiting for connection...')" > /dev/null 2>&1; do
   sleep 2
